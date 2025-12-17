092892f56b5f2e103c1b86ba7918cac167594b4f
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2025-12-17 16:40:14 -0600

    Se corrigio la task 5

--- src/task5.js
+++ src/task5.js
@@ -2,9 +2,9 @@ const arr0 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
 // Type your code below this line!
 
-arr0.join()
 
-console.log(arr0)
+
+console.log(arr0.join(","))
 
 // Type your code above this line!
 console.log("hola")

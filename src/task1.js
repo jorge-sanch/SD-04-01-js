dea3da7de92b87eac85c75412b818f7a3defb0b4
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2025-12-17 16:00:21 -0600

    Se resolvieron el test 1, 2, 3, 4

--- src/task1.js
+++ src/task1.js
@@ -3,6 +3,27 @@ const arr = []
 // Escribe tu codigo por debajo de esta linea
 
 
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+arr.push(6);
+arr.push(7);
+arr.push(8);
+arr.push(9);
+arr.push(10);
+arr.push(11);
+arr.push(12);
+arr.push(13);
+arr.push(14);
+arr.push(15);
+arr.push(16);
+arr.push(17);
+arr.push(18);
+arr.push(19);
+arr.push(20);
+
 
 // Escribe tu codigo por encima de esta linea
 
